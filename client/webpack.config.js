71898b13e585b38b83522bf024e5d8cdf26dc323
BraxtonM <braxtoncoding@gmail.com>
2023-01-08 21:47:24 -0500

    worked on webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,19 +19,49 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-
+        template: './index.html',
+        title: 'J.A.T.E'
       }),
       new InjectManifest({
-
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
-
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Take notes with Javascript!',
+        background_color: '', // add color
+        theme_color: '',  // add color
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          { // add sizes for icons
+            src: path.resolve('src/images/logo.png'),
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
